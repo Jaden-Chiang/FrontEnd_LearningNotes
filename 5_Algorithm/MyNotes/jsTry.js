b83fef5a3ae102924ddd7d52c98782f91a37d4b2
Jaden-Chiang <Jaden-Chiang@users.noreply.github.com>
2022-07-05 23:41:11 +0800

    add notes

---
+++ 5_Algorithm/MyNotes/jsTry.js
@@ -0,0 +1,10 @@
+// var list = [1, 2, 3, 4, 5];
+// var a = list.splice(2);
+
+// console.log(a);
+// console.log(list);
+
+var list = [[1], [2, 3]];
+list.push([4, 5, 6]);
+console.log(list);
+console.log(typeof (list));
