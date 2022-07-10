fbe5936e5b8f541121be219c9a2a6a111ca40ced
Jaden-Chiang <Jaden-Chiang@users.noreply.github.com>
2022-07-11 00:15:03 +0800

    add notes

--- 5_Algorithm/MyNotes/jsTry.js
+++ 5_Algorithm/MyNotes/jsTry.js
@@ -1,10 +1,24 @@
-// var list = [1, 2, 3, 4, 5];
-// var a = list.splice(2);
+nums = [-2, -2, 1, 1, 4, 1, 4, 4, -4, -2];
 
-// console.log(a);
-// console.log(list);
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    nums.sort();
+    console.log(nums)
 
-var list = [[1], [2, 3]];
-list.push([4, 5, 6]);
-console.log(list);
-console.log(typeof (list));
+    for (var i = 0; i < nums.length; i++) {
+        // 如果i位置的元素和i+1位置的元素相等，直接跳过
+        if (nums[i] == nums[i + 1]) {
+            i += 2;
+        } else if (i = nums.length - 1) {
+            return nums[i];
+        } else {
+            return nums[i];
+        }
+    }
+};
+
+var res = singleNumber(nums);
+console.log(res);
