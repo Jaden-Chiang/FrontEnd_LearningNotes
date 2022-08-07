e06c375991bd3c7cb8c5c8fa98027eef43483055
Jaden-Chiang <Jaden-Chiang@users.noreply.github.com>
2022-08-07 11:56:47 +0800

    add notes

--- 5_Algorithm/MyNotes/jsTry copy.js
+++ 5_Algorithm/MyNotes/jsTry copy.js
@@ -1,14 +1,6 @@
-/**
- * @param {string} beginWord
- * @param {string} endWord
- * @param {string[]} wordList
- * @return {number}
- */
-
-let beginWord = "hot";
-let endWord = "dog";
-let wordList = ["cog", "dog", "tot", "hog", "hop", "pot", "dot"];
-
+var beginWord = "cet";
+var endWord = "ism";
+var wordList = ["kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"];
 
 /**
  * @param {string} beginWord
@@ -20,6 +12,8 @@ var ladderLength = function (beginWord, endWord, wordList) {
 
     // 获取单词的长度，本题中len是常量
     const len = beginWord.length;
+    // 根据wordList创建结合set
+    const wordSet = new Set(wordList);
 
     // 判断两个单词是否只相差一个字母
     var getDiff = function (a, b) {
@@ -31,47 +25,26 @@ var ladderLength = function (beginWord, endWord, wordList) {
         return true;
     };
 
-    /*
-    * @param{array} findlist: 需要查找的单词列表
-    * @param{set} wordSet: 目前有的单词集合
-    * @param{number} res: 结果
-    * @return{number} res
-     */
-    var findWay = function (findList, wordSet, endWord, result) {
-        while (wordSet.size != 0) {
-            result++;
-            let findTimes = findList.length;
-            for (var i = 0; i < findTimes; i++) {
-                for (var checkWord of wordSet.keys()) {
-                    if (getDiff(checkWord, findList[i])) {
-                        findList.push(checkWord);
-                        wordSet.delete(checkWord);
-                    }
-                }
-            }
-            findList.splice(0, findTimes);
-        }
-
-        for (var i = 0; i < findList.length; i++) {
-            if (getDiff(endWord, findList[i])) {
-                result += 2;
-                return result;
-            }
-        }
-
-        return 0;
-    };
-
-    // 根据单词列表创建集合
-    const wordSet = new Set(wordList);
-    let res = 0;
-    // 如果endWord不在wordList中，直接返回0，否则将直接删除
-    if (wordSet.delete(endWord) === false) return res;
-    // 如果beginWord和endWord直接符合条件，则返回2
+    // 如果wordList为0或者不含有endWord，直接返回0
+    if (wordList.length == 0 || !wordSet.has(endWord)) return 0;
+    // 如果beginWord和endWord符合条件，直接返回2
     if (getDiff(beginWord, endWord)) return 2;
-    res = findWay([beginWord], wordSet, endWord, res);
 
-    return res;
+    // 创建检索数组，并将beginWord入列，并将层数加入
+    const findList = [[beginWord, 1]];
+    while (findList.length != 0) {
+        let checkWord = findList.pop();
+        if (checkWord[0] == endWord) return checkWord[1];
+        for (var word of wordSet) {
+            // 如果符合条件，则加入检索数组。并将层数加1
+            if (getDiff(checkWord[0], word)) {
+                findList.push([word, (checkWord[1] + 1)]);
+                wordSet.delete(word);
+            }
+        }
+    }
+
+    return 0;
 };
 
 console.log(ladderLength(beginWord, endWord, wordList));
--- 5_Algorithm/MyNotes/jsTry.js
+++ 5_Algorithm/MyNotes/jsTry.js
@@ -1,12 +1,12 @@
-// var getDiff = function (a, b) {
-//     const len = a.length;
-//     let flag = 0;
-//     for (var i = 0; i < len; i++) {
-//         if (a.charAt(i) != b.charAt(i)) flag++;
-//         if (flag > 1) return false;
-//     }
-//     return true;
-// };
+var getDiff = function (a, b) {
+    const len = a.length;
+    let flag = 0;
+    for (var i = 0; i < len; i++) {
+        if (a.charAt(i) != b.charAt(i)) flag++;
+        if (flag > 1) return false;
+    }
+    return true;
+};
 
 // var isContain = function (a, list) {
 //     for (var i = 0; i < list.length; i++) {
@@ -15,7 +15,7 @@
 //     return false;
 // };
 
-var list = ["lit", "lot", "let"];
+// var list = 
 
 // var isContain = function (a, list) {
 //     let result = list.filter(checkContain);
@@ -47,8 +47,30 @@ var list = ["lit", "lot", "let"];
 //     console.log(key);
 // }
 
-var test = function (num) {
-    return (num++);
-};
+// var test = function (num) {
+//     return (num++);
+// };
 
-console.log(test(1));
+// console.log(test(1));
+
+var findList = ['cet', 'cot', 'col', 'dot', 'lot', 'pot', 'cat', 'cox',
+    'cod', 'cog', 'con', 'tot', 'wot', 'cob', 'com', 'hot',
+    'not', 'coo', 'coy', 'jot', 'got', 'cet', 'yet', 'yea',
+    'set', 'let', 'vet', 'bet', 'yep', 'pet', 'yew', 'get',
+    'net', 'wet', 'met', 'cet', 'cot', 'fat', 'cal', 'cat',
+    'can', 'vat', 'eat', 'cap', 'bat', 'tat', 'nat', 'caw',
+    'cad', 'cab', 'pat', 'cet', 'sew', 'yet', 'set', 'sec',
+    'let', 'see', 'vet', 'bet', 'sen', 'pet', 'sgt', 'get',
+    'net', 'wet', 'met', 'cet', 'lit', 'led', 'yet', 'lot',
+    'set', 'let', 'vet', 'bet', 'leg', 'pet', 'get', 'lei',
+    'net', 'wet', 'met', 'len', 'cet', 'yet', 'set', 'vat',
+    'let', 'vet', 'bet', 'pet', 'get', 'net', 'wet', 'met',
+    'cet', 'yet', 'but', 'bee',];
+var result = [];
+for (var i = 0; i < findList.length; i++) {
+    console.log(findList[i])
+    for (var j = 0; j < list.length; j++) {
+        if (getDiff(findList[i], list[j])) result.push(list[j]);
+    }
+}
+console.log(result);
