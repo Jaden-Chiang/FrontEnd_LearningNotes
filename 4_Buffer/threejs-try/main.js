371a6a013cbae1a84e5337d092680ef34b52b0af
Jaden-Chiang <Jaden-Chiang@users.noreply.github.com>
2023-05-17 01:03:26 +0800

    add some threeJS files

--- 4_Buffer/threejs-try/main.js
+++ 4_Buffer/threejs-try/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -11,29 +12,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
+loader.load("/roman_pottery_workshop_-_free_download.glb", function (object) {
+  scene.add(object.scene);
+});
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
 
 camera.position.z = 5;
 
-// create  a line
-const rendererLine = new THREE.WebGLRenderer();
-rendererLine.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(rendererLine.domElement);
-const cameraLine = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-cameraLine.position.set(0, 0, 100);
-cameraLine.lookAt(0, 0, 0);
-const sceneLine = new THREE.Scene();
-const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
-
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
   renderer.render(scene, camera);
 }
-
 animate();
-
